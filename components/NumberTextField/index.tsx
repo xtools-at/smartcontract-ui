25b8a9beb30640691a10a8a75c65d646b8a87afa
Martin Kainzbauer <m.kainzbauer@xtools.at>
2023-10-20 16:45:46 +0200

    chore: package version, formatting

--- components/NumberTextField/index.tsx
+++ components/NumberTextField/index.tsx
@@ -1,35 +1,36 @@
-import { forwardRef, useState, useCallback, useEffect } from 'react'
-import TextField from '@mui/material/TextField'
-import InputAdornment from '@mui/material/InputAdornment'
-import NumberFormat from 'react-number-format'
+import { forwardRef, useState, useCallback, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import NumberFormat from "react-number-format";
 
 interface NumberFormatComponentProps {
 	onChange: (event: { target: { name: string; value: string } }) => void;
 	name: string;
 }
 
-const NumberFormatComponent = forwardRef<NumberFormat, NumberFormatComponentProps>(
-	function NumberFormatCustom(props, ref) {
-		const { onChange, ...other } = props
+const NumberFormatComponent = forwardRef<
+	NumberFormat,
+	NumberFormatComponentProps
+>(function NumberFormatCustom(props, ref) {
+	const { onChange, ...other } = props;
 
-		return (
-			<NumberFormat
-				{...other}
-				getInputRef={ref}
-				onValueChange={(values) => {
-					onChange({
-						target: {
-							name: props.name,
-							value: values.value,
-						},
-					})
-				}}
-				thousandSeparator
-				isNumericString
-			/>
-		)
-	},
-)
+	return (
+		<NumberFormat
+			{...other}
+			getInputRef={ref}
+			onValueChange={(values) => {
+				onChange({
+					target: {
+						name: props.name,
+						value: values.value,
+					},
+				});
+			}}
+			thousandSeparator
+			isNumericString
+		/>
+	);
+});
 
 export const NumberTextField = ({
 	value,
@@ -37,37 +38,34 @@ export const NumberTextField = ({
 	onChange,
 	...props
 }: {
-	value: any,
-	decimalDisabled?: boolean,
-	onChange: (event: any) => void
+	value: any;
+	decimalDisabled?: boolean;
+	onChange: (event: any) => void;
 } & any) => {
-	const [number, setNumber] = useState('')
-	const [decimal, setDecimal] = useState('')
-
+	const [number, setNumber] = useState("");
+	const [decimal, setDecimal] = useState("");
 
 	const triggerOnChange = useCallback((_number, _decimal) => {
-		const value = `${_number}e${_decimal || 0}`
-		onChange(value)
-	}, [])
+		const value = `${_number}e${_decimal || 0}`;
+		onChange(value);
+	}, []);
 
 	useEffect(() => {
-		const valueString = String(value || '')
-		const valueComponents = valueString.split('e')
+		const valueString = String(value || "");
+		const valueComponents = valueString.split("e");
 
-		setNumber(valueComponents[0])
-		setDecimal(valueComponents[1] || '0')
-	}, [value])
-
-
+		setNumber(valueComponents[0]);
+		setDecimal(valueComponents[1] || "0");
+	}, [value]);
 
 	return (
 		<TextField
 			// type='number'
 			// value={value}
 			value={number}
-			onChange={event => {
-				setNumber(event.target.value)
-				triggerOnChange(event.target.value, decimal)
+			onChange={(event) => {
+				setNumber(event.target.value);
+				triggerOnChange(event.target.value, decimal);
 			}}
 			InputProps={{
 				inputComponent: NumberFormatComponent as any,
@@ -76,22 +74,24 @@ export const NumberTextField = ({
 					<>
 						<TextField
 							sx={{
-								height: '10px',
-								marginTop: '-24px',
-								width: '160px'
+								height: "10px",
+								marginTop: "-24px",
+								width: "160px",
 							}}
 							disabled={decimalDisabled}
-							type='number'
-							margin='normal'
-							size='small'
+							type="number"
+							margin="normal"
+							size="small"
 							value={decimal}
-							onChange={event => {
-								setDecimal(event.target.value)
+							onChange={(event) => {
+								setDecimal(event.target.value);
 
-								triggerOnChange(number, event.target.value)
+								triggerOnChange(number, event.target.value);
 							}}
 							InputProps={{
-								startAdornment: <InputAdornment position='start'>x 10 ^ </InputAdornment>,
+								startAdornment: (
+									<InputAdornment position="start">x 10 ^ </InputAdornment>
+								),
 							}}
 						/>
 					</>
@@ -99,5 +99,5 @@ export const NumberTextField = ({
 			}}
 			{...props}
 		/>
-	)
-}
+	);
+};
