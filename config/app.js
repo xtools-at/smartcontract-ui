13c9b13649f04571feaa21ffdbd7fc85a000e8b9
Martin Kainzbauer <m.kainzbauer@xtools.at>
2023-10-23 00:50:04 +0200

    chore: force build

--- config/app.js
+++ config/app.js
@@ -1,7 +1,8 @@
 module.exports = {
 	name: "Smart Contract UI",
 	title: "Smart Contract UI | Ethereum contract interaction tool",
-	description: "Interact with any Ethereum/EVM smart contract with ease.",
+	description:
+		"Interact with any Ethereum/EVM smart contract with ease. Built for Beam network.",
 	basePath: "/smartcontract-ui", // set to "" if not needed
 	host: "https://xtools-at.github.io",
 	corsProxyUrlPrefix: "https://corsproxy.io/?",
